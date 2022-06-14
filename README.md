292427471be355421d191d797180b12feda70634
Hoang Nguyen <hoangng210a@gmail.com>
2022-06-14 22:16:10 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# my data science environment
+
+this is a helper README file with information from various sources. 
---
+++ test.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import numpy as np
+
+
+print("HELLO WORLD")
+
+d = {
+}
+
+def  my_function():
+    pass 
+
+def  wtf():
+    pass
